dc04322e1cfebffca79712842eab0ef1840eeb3b
Gaurav Juvekar <gauravjuvekar@gmail.com>
2018-03-05 17:20:38 +0530

    Add basic SIF and s2v embedding printing

---
+++ wsd.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import SIF
+import SIF.data_io
+import SIF.SIF_embedding
+import SIF.params
+import sent2vec
+
+import scipy
+import scipy.spatial
+import scipy.spatial.distance
+
+ENABLE = ('sif', 's2v')
+
+sif_db = SIF.data_io.setup_db('./data/sif.db')
+s2v_model = sent2vec.Sent2vecModel()
+
+if 's2v' in ENABLE:
+    s2v_model.load_model('./data/s2v_wiki_unigrams.bin')
+
+def sif_embeds(sent_list):
+    idx_mat, weight_mat, data = data_io.prepare_data(sent_list sif_db)
+    params = SIF.params.params()
+    params.rmpc = 1
+    embedding = SIF.SIF_embedding.SIF_embedding(idx_mat,
+                                                weight_mat,
+                                                data,
+                                                params)
+    return list(embedding)
+
+def s2v_embeds(sent_list):
+    return [s2v_model.embed_sentence(' '.join(sent)) for sent in sent_list]
+
+
+if __name__ == '__main__':
+    sentences = ["this is a sentence".split(),
+                 "and this is another one".split()]
+
+    import pprint
+    print(Sentences)
+    pprint.pprint(sentences)
+    print("SIF embeddings")
+    pprint.pprint(sif_embeds(sentences))
+    print("S2V embeddings")
+    pprint.pprint(s2v_embeds(sentences))
+
+
+
